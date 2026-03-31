d68d5a487e541a1a212d8ee5c6c9e7b5146a27b9
Nicolas Lemoine <nico.lemoine@gmail.com>
2026-03-31 14:42:28 +0200

    fix: Correct function name from load_theme_textdomain to load_textdomain

--- autoload.php
+++ autoload.php
@@ -5,5 +5,5 @@ use n5s\WpHookKit\Hook;
 Hook::addAction('after_setup_theme', static function() {
 	require_once 'lib/functions.php';
 
-	load_theme_textdomain( 'mind/types', __DIR__ . '/languages/types-' . determine_locale() . '.mo' );
+	load_textdomain( 'mind/types', __DIR__ . '/languages/types-' . determine_locale() . '.mo' );
 }, 1);
