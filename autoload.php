db84e81d198e7c27096d2e1768c69c63e0d14aed
Nicolas Lemoine <nico.lemoine@gmail.com>
2026-03-31 11:45:34 +0200

    feat: Use early hook to make loading languges work in all cases (#16)
    Fixes #14

--- autoload.php
+++ autoload.php
@@ -1,9 +1,9 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	return;
-}
+use n5s\WpHookKit\Hook;
 
-require_once 'lib/functions.php';
+Hook::addAction('after_setup_theme', static function() {
+	require_once 'lib/functions.php';
 
-load_textdomain( 'mind/types', __DIR__ . '/languages/types-' . determine_locale() . '.mo' );
+	load_theme_textdomain( 'mind/types', __DIR__ . '/languages/types-' . determine_locale() . '.mo' );
+}, 1);
